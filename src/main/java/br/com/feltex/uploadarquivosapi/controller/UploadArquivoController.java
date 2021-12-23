09cabe8239d15aff2d6898d951443c90edc28b3a
Andre Felix <andrefelix.br@gmail.com>
2021-12-23 18:11:52 +0000

    Adicionar ClienteDto

--- src/main/java/br/com/feltex/uploadarquivosapi/controller/UploadArquivoController.java
+++ src/main/java/br/com/feltex/uploadarquivosapi/controller/UploadArquivoController.java
@@ -1,5 +1,8 @@
 package br.com.feltex.uploadarquivosapi.controller;
 
+import br.com.feltex.uploadarquivosapi.controller.dto.ClienteDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -10,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.text.MessageFormat;
 import java.util.UUID;
 
 @RestController
@@ -19,8 +23,9 @@ import java.util.UUID;
 public class UploadArquivoController {
 
     private final String pathArquivos;
+    private final ObjectMapper mapper = new ObjectMapper();
 
-    public UploadArquivoController(@Value("${app.path.arquivos}") String pathArquivos){
+    public UploadArquivoController(@Value("${app.path.arquivos}") String pathArquivos) {
         this.pathArquivos = pathArquivos;
     }
 
@@ -37,6 +42,26 @@ public class UploadArquivoController {
         } catch (Exception e) {
             log.error("Erro ao processar arquivo", e);
             return new ResponseEntity<>("{ \"mensagem\": \"Erro ao carregar o arquivo!\"}", HttpStatus.OK);
+        }
+    }
+
+
+    @PostMapping("/cliente")
+    public ResponseEntity<String> salvarArquivoComMetadata(@RequestParam("file") final MultipartFile file,
+                                                           @RequestParam("cliente") final String clienteData) throws JsonProcessingException {
+        log.info("Recebendo arquivo " + file.getOriginalFilename());
+
+        final var cliente = mapper.readValue(clienteData, ClienteDto.class);
+
+        log.info("Dados do cliente {}", cliente);
+
+        var caminho = MessageFormat.format("{0}{1}-{2}.{3}", pathArquivos,
+                cliente.getNome(), UUID.randomUUID(), extrairExtensao(file.getOriginalFilename()));
+        try {
+            Files.copy(file.getInputStream(), Path.of(caminho), StandardCopyOption.REPLACE_EXISTING);
+            return new ResponseEntity<>("{ \"message\": \"Arquivo carregado com Sucesso!\"}", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("{ \"message\":  \"Erro ao carregar o arquivo!\"}", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
---
+++ src/main/java/br/com/feltex/uploadarquivosapi/controller/dto/ClienteDto.java
@@ -0,0 +1,9 @@
+package br.com.feltex.uploadarquivosapi.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class ClienteDto {
+    private String nome;
+    private String telefone;
+}
